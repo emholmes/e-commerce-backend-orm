832625ab78d0b6e6c09021ea347abca053264f60
Erin Holmes <e.holmes09@yahoo.com>
2021-08-08 11:45:52 -0500

    add cpost request for tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -66,10 +66,17 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create(req.body)
+  .then(dbTagData => res.json(dbTagData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  
 });
 
 router.delete('/:id', (req, res) => {
